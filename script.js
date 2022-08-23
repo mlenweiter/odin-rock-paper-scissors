5c8d7507180916a4167273ee7dd942dbf8354d91
mlenweiter <martinlenweiter@protonmail.com>
2022-08-23 12:32:05 +0200

    works

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-console.log("hello")
+setTimeout(function(){
+    //do what you need here
+}, 20000);
+
+console.log("hello!!")
+
+
+
 
 let finished = false
 let computerScore = 0;
@@ -6,70 +13,96 @@ let userScore = 0;
 //TODO: prompt the user for max score
 let maxScore = 5;
 
+
+
 function getComputerChoice(){
-    ['r', 'p', 's'][Math.floor(Math.random()*3)]
+    return ['r', 'p', 's'][Math.floor(Math.random()*3)]
 
 }
 
-while(finished===false){
+;
+
+
+while(!finished){
     // get the computer's choice
+    let userChoice;
     let computerChoice = getComputerChoice();
     // get the user's choice
-    let userChoice;
+
     while(userChoice != 'r' && userChoice != 'p' && userChoice != 's') {
         userChoice = prompt("Enter your choice (r | p | s)");
     }
-    // compute the winner
+
+    console.log("Computer: " + computerChoice)
+    console.log("You: " + userChoice)
+
+    
 
     switch (userChoice) {
         case 'r' : 
             switch(computerChoice){
                 case 'r' :
-                    console.log("TIE!");      
+                    console.log("TIE!"); 
+                    break     
                 case 'p' :
-                    console.log("Computer Wins!");    
                     computerScore++;  
+                    console.log("Computer Wins! Computer score is: " + computerScore); 
+                    break
                 case 's' :
-                    console.log("You Win!!"); 
-                    userScore++;  
+                    userScore++
+                    console.log("You Win! Your score is: " + userScore); 
+                    break
+                 
 
             }
+            break
 
         case 'p' :
             switch(computerChoice){
                 case 'r' :
-                    console.log("You Win!!"); 
-                    userScore++;
+                    userScore++
+                    console.log("You Win! Your score is: " + userScore); 
+                    break
                 case 'p' :
                     console.log("TIE!");      
+                    break
 
                 case 's' :
-                    console.log("Computer Wins!"); 
                     computerScore++;  
+                    console.log("Computer Wins! Computer score is: " + computerScore); 
+                    break
+
                 
             }
+            break
         case 's' :
             switch(computerChoice){
                 case 'r' :
-                    console.log("Computer Wins!"); 
                     computerScore++;  
+                    console.log("Computer Wins! Computer score is: " + computerScore); 
+                    break 
                 case 'p' :
-                    console.log("You Win!!"); 
                     userScore++;
+                    console.log("You Win! Your score is: " + userScore); 
+                    break
                 case 's' :
                     console.log("TIE!");      
+                    break
 
             }
+            break
     }
     // if there's a winner, end.
-    if (userScore === 5){
+    if (userScore >= 5){
         console.log("You have won. Congratulations.");
         finished = true;
+        
     }
 
-    if (computerScore === 5){
+    if (computerScore >= 5){
         console.log("Computer has won. Congratulations computer.");
         finished = true;
+        
     }
 
 }
